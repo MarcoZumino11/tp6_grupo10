19f3bb84ba1e99c39e8153114eedc5601c4a7eff
Lucas Luciano Camino <capitanchorizo1234@gmail.com>
2025-11-11 20:26:17 -0300

    Exceptions

--- src/ar/edu/unju/escmi/tp6/exceptions/LibroNoEncontradoException.java
+++ src/ar/edu/unju/escmi/tp6/exceptions/LibroNoEncontradoException.java
@@ -1,5 +1,28 @@
 package ar.edu.unju.escmi.tp6.exceptions;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Excepción lanzada cuando el sistema no encuentra un libro según el ID.
+ * 
+ * 
+ * Incluye atributos y métodos propios para brindar contexto adicional.
+ */
 public class LibroNoEncontradoException extends Exception {
-    public LibroNoEncontradoException(String mensaje) { super(mensaje); }
+    private LocalDateTime fechaError;
+
+    public LibroNoEncontradoException(String mensaje) {
+        super(mensaje);
+        this.fechaError = LocalDateTime.now();
+    }
+
+    public void mostrarError() {
+        String hora = fechaError.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
+        System.out.println("[LibroNoEncontradoException] " + getMessage() + " | Hora: " + hora);
+    }
+
+    public LocalDateTime getFechaError() {
+        return fechaError;
+    }
 }
